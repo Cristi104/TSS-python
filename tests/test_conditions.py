e53941fcc9577796985d01dc5a87c5663d234e03
cristi <petrecristian2004@gmail.com>
2026-05-03 19:31:34 +0300

    fix structural tests

--- tests/test_conditions.py
+++ tests/test_conditions.py
@@ -2,59 +2,123 @@ import pytest
 from app.student import Student
 from app.ui import ui
 
-def test_no_students():
-    app = ui()
-    app.students = None
-    output = app.generate_report()
-    assert "NO_DATA" in output
 
-def test_student_with_9():
+## ui.menu
+def test_0(monkeypatch, capsys):
     app = ui()
-    app.students = [Student(1, "A", [9, 9, 9])]
-    output = app.generate_report()
-    assert output["total"] == 1
-    assert output["passing"] == 1
-    assert output["avg"] == 9
-    assert output["performance"] == "HIGH"
-    assert output["top"] == "A"
+    inputs = iter(["0"])
+    monkeypatch.setattr("builtins.input", lambda *args: "0")
+    app.menu()
+    captured = capsys.readouterr()
+    assert "\n1 - show students\n2 - add student\n3 - remove student\n4 - add grade\n5 - generate report\n6 - filter students by average\n0 - exit\n" in captured.out
+    
+def test_1(monkeypatch, capsys):
+    app = ui()
+    inputs = iter(["1", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "student1" in captured.out
+    assert "student2" in captured.out
+    assert "student3" in captured.out
 
-def test_student_with_6():
+def test_2(monkeypatch, capsys):
     app = ui()
-    app.students = [Student(1, "A", [6, 6, 6])]
-    output = app.generate_report()
-    assert output["total"] == 1
-    assert output["passing"] == 1
-    assert output["avg"] == 6
-    assert output["performance"] == "MEDIUM"
-    assert output["top"] == "A"
+    inputs = iter(["2", "nume 5", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "Format: <name> [grade1 [grade2 [...]]]" in captured.out
+    
+def test_3(monkeypatch, capsys):
+    app = ui()
+    inputs = iter(["3", "0", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "student1" in captured.out
+    assert "student2" in captured.out
+    assert "student3" in captured.out
+    assert "Student id:" in captured.out
+    
+def test_4(monkeypatch, capsys):
+    app = ui()
+    inputs = iter(["4", "0 5", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "student1" in captured.out
+    assert "student2" in captured.out
+    assert "student3" in captured.out
+    assert "Format: <id> <grade>" in captured.out
+    
+def test_5(monkeypatch, capsys):
+    app = ui()
+    inputs = iter(["5", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "REPORT" in captured.out
+    assert "{'total': 3, 'passing': 2, 'avg': 4.266666666666667, 'performance': 'LOW', 'top': 'student3'}" in captured.out
 
-def test_student_with_4():
+def test_6(monkeypatch, capsys):
     app = ui()
-    app.students = [Student(1, "A", [4, 4, 4])]
-    output = app.generate_report()
-    assert output["total"] == 1
-    assert output["passing"] == 0
-    assert output["avg"] == 4
-    assert output["performance"] == "LOW"
-    assert output["top"] == "A"
+    app.students = [Student(1, "A", [9, 9, 9]), Student(2, "B", [6, 6, 6]), Student(3, "C", [3, 3, 3])]
+    inputs = iter(["6", "5 10", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "Format: <min_avg> <max_avg>" in captured.out
+    assert "A" in captured.out
+    assert "B" in captured.out
+    assert "FILTERED" in captured.out
 
-def test_student_with_8_9():
+def test_6_empty(monkeypatch, capsys):
     app = ui()
-    app.students = [Student(1, "A", [8, 8, 8]), Student(2, "B", [9, 9, 9])]
-    output = app.generate_report()
-    assert output["total"] == 2
-    assert output["passing"] == 2
-    assert output["avg"] == 8.5
-    assert output["performance"] == "HIGH"
-    assert output["top"] == "B"
+    app.students = []
+    inputs = iter(["6", "0 10", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "No students found" in captured.out
 
-def test_student_with_9_8():
+def test_6_invalid_input(monkeypatch, capsys):
     app = ui()
-    app.students = [Student(1, "A", [9, 9, 9]), Student(2, "B", [8, 8, 8])]
-    output = app.generate_report()
-    assert output["total"] == 2
-    assert output["passing"] == 2
-    assert output["avg"] == 8.5
-    assert output["performance"] == "HIGH"
-    assert output["top"] == "A"
+    inputs = iter(["6", "invalid", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "Invalid input" in captured.out
+
+def test_6_invalid_range(monkeypatch, capsys):
+    app = ui()
+    inputs = iter(["6", "6 5", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "Invalid range" in captured.out
+
+def test_7(monkeypatch, capsys):
+    app = ui()
+    inputs = iter(["7", "0"])
+    monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
+    app.menu()
+    captured = capsys.readouterr()
+    assert "\n1 - show students\n2 - add student\n3 - remove student\n4 - add grade\n5 - generate report\n6 - filter students by average\n0 - exit\n" in captured.out
+
+## ui.filter_students
+def test_6_4():
+    app = ui()
+    with pytest.raises(ValueError):
+        result = app.filter_students(6, 4)
+
+def test_1_2():
+    app = ui()
+    result = app.filter_students(1, 2)
+    assert len(result) == 0
+
+def test_9_10():
+    app = ui()
+    result = app.filter_students(9, 10)
+    assert len(result) == 0
 
--- tests/test_statement.py
+++ tests/test_statement.py
@@ -2,11 +2,15 @@ import pytest
 from app.student import Student
 from app.ui import ui
 
-def test_0(monkeypatch):
+
+## ui.menu
+def test_0(monkeypatch, capsys):
     app = ui()
     inputs = iter(["0"])
     monkeypatch.setattr("builtins.input", lambda *args: "0")
     app.menu()
+    captured = capsys.readouterr()
+    assert "\n1 - show students\n2 - add student\n3 - remove student\n4 - add grade\n5 - generate report\n6 - filter students by average\n0 - exit\n" in captured.out
     
 def test_1(monkeypatch, capsys):
     app = ui()
@@ -14,7 +18,9 @@ def test_1(monkeypatch, capsys):
     monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
     app.menu()
     captured = capsys.readouterr()
-    assert "Student" in captured.out
+    assert "student1" in captured.out
+    assert "student2" in captured.out
+    assert "student3" in captured.out
 
 def test_2(monkeypatch, capsys):
     app = ui()
@@ -30,6 +36,9 @@ def test_3(monkeypatch, capsys):
     monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
     app.menu()
     captured = capsys.readouterr()
+    assert "student1" in captured.out
+    assert "student2" in captured.out
+    assert "student3" in captured.out
     assert "Student id:" in captured.out
     
 def test_4(monkeypatch, capsys):
@@ -38,6 +47,9 @@ def test_4(monkeypatch, capsys):
     monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
     app.menu()
     captured = capsys.readouterr()
+    assert "student1" in captured.out
+    assert "student2" in captured.out
+    assert "student3" in captured.out
     assert "Format: <id> <grade>" in captured.out
     
 def test_5(monkeypatch, capsys):
@@ -47,6 +59,7 @@ def test_5(monkeypatch, capsys):
     app.menu()
     captured = capsys.readouterr()
     assert "REPORT" in captured.out
+    assert "{'total': 3, 'passing': 2, 'avg': 4.266666666666667, 'performance': 'LOW', 'top': 'student3'}" in captured.out
 
 def test_6(monkeypatch, capsys):
     app = ui()
@@ -55,6 +68,7 @@ def test_6(monkeypatch, capsys):
     monkeypatch.setattr("builtins.input", lambda *args: next(inputs))
     app.menu()
     captured = capsys.readouterr()
+    assert "Format: <min_avg> <max_avg>" in captured.out
     assert "A" in captured.out
     assert "B" in captured.out
     assert "FILTERED" in captured.out
@@ -83,3 +97,14 @@ def test_6_invalid_range(monkeypatch, capsys):
     app.menu()
     captured = capsys.readouterr()
     assert "Invalid range" in captured.out
+
+## ui.filter_students
+def test_4_6():
+    app = ui()
+    result = app.filter_students(4, 6)
+    assert len(result) != 0
+
+def test_6_4():
+    app = ui()
+    with pytest.raises(ValueError):
+        result = app.filter_students(6, 4)
